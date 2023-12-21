ea80fbf17245497883ab281a29a595696c676c2c
Xiriuaex <pragyanchetia22@gmail.com>
2023-12-21 13:00:52 +0530

    minor fixes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ import "@/styles/Hero.css";
 const Hero = () => {
   return (
     <div>
-      <video className="relative right-0 -top-20 object-fill w-full h-[100vh]" src={vidSrc} playsInline autoPlay muted loop type="video/mp4" />
+      <video className="hero relative right-0 -top-20 object-fill w-full h-[100vh]" src={vidSrc} playsInline autoPlay muted loop type="video/mp4" />
       <div className='heading'>
         <h1 className='mobile:text-4xl tablet:text-5xl laptop:text-6xl'>CHAWDANG CHINMOY</h1>
         <h1 className='mobile:text-xl tablet:text-3xl font-semibold'>Enthusiast Filmmaker</h1>
--- data/dataContext.jsx
+++ data/dataContext.jsx
@@ -3,7 +3,7 @@ export const videoData= [
         embededCode: 'FShDBond7Zg',
         title: 'Girls Volleyball Tournament',
         description: "A girl's Vollyball match organized by PBH",
-        category: 'promo',
+        category: 'Promo',
     },
     {
         embededCode: 'dF4Gni4ykbc',
@@ -15,36 +15,36 @@ export const videoData= [
         embededCode: 'lRBCxzauFn8',
         title: 'Udayanmukh 3.0',
         description: 'Udayanmukh 3.0 Aftermovie by PBH',
-        category: 'promo',
+        category: 'Promo',
     },
     {
         embededCode: '3svT2Bdqfls',
         title: 'The Spirit Of Bihu',
         description: 'Bihu is emotion',
-        category: 'promo',
+        category: 'Promo',
     },
     {
         embededCode: 'pHG5pz_YMVE',
         title: "Let's Break The Chain",
         description: "The inheritance of ragging. Let's brake the chain once and for all",
-        category: 'short film',
+        category: 'Short film',
     },
     {
         embededCode: '-Y0rr4caSjE',
         title: 'ATMOKOTHA',
         description: '',
-        category: 'music video',
+        category: 'Music video',
     },
     {
         embededCode: 'sT_UUVJBXCg',
         title: 'CODE 171',
         description: '',
-        category: 'music video',
+        category: 'Music video',
     },
     {
         embededCode: 'BZz4XOhulcE',
         title: 'BPKAE',
         description: 'Love can be hard',
-        category: 'short film',
+        category: 'Short film',
     }
 ]
